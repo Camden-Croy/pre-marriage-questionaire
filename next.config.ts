d37597f824855f34647e26c73d12159543cf9211
Camden Croy <camdencroy4@gmail.com>
2026-01-31 11:48:38 -0500

    tip tap issue

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   turbopack: {},
+  transpilePackages: [
+    "@tiptap/core",
+    "@tiptap/react",
+    "@tiptap/pm",
+    "@tiptap/starter-kit",
+    "@tiptap/extension-placeholder",
+    "@tiptap/extension-floating-menu",
+  ],
 };
 
 export default nextConfig;
